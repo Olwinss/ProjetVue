4a2cf50c45a077b2cbcbdeb894a11d9b860e4d6a
Florian9543 <imbaud.florian@orange.fr>
2024-02-20 15:48:01 +0100

    Ajout du prototype de Router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import Potions from './components/Potions.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-link to="/">Go to Home</router-link>
+
+  <router-link to="/potions">Go to Potions</router-link>
+  <router-link to="/spells">Go to Spells</router-link>
+
+  <router-view></router-view>
 </template>
 
 <style scoped>
