535647b789f68812f442af1d9b925514ea912ca0
Florian9543 <imbaud.florian@orange.fr>
2024-03-20 17:27:50 +0100

    Ajout d'une image par défaut et de l'affichage des images correctes Ajoute de l'affichage des effets et ingrédients, si ils sont présents dans la BDD

--- src/components/PotionsDetails.vue
+++ src/components/PotionsDetails.vue
@@ -2,13 +2,25 @@
     <div id="HeaderContent">
         <div id="title">
             <h1> Page de la potion :</h1>
+            <img v-if="potion.data.attributes.image !== null" :src="potion.data.attributes.image" :alt="potion.data.attributes.name" class="potion-image"/>
+            <img v-else src="../assets/missing_potion.svg" alt="Missing Potion" class="potion-image"/>
             <h2 v-if="isPotionLoaded">Nom de la potion : {{ potion.data.attributes.name }}</h2>
             <div v-else>
                 Chargement en cours...
             </div>
+            <p v-if="isPotionLoaded && potion.data.attributes.effect !== null">Effet : {{ potion.data.attributes.effect }}</p>
+            <p v-if="isPotionLoaded && potion.data.attributes.ingredients !== null">Ingrédients : {{ potion.data.attributes.ingredients }}</p>
+
         </div>
     </div>
 </template>
+
+<style>
+.potion-image {
+    max-width: 400px;
+    max-height: 400px;
+}
+</style>
 
 <script setup>
 import { ref, onMounted } from 'vue';
@@ -35,5 +47,3 @@ axios.get('https://api.potterdb.com/v1/potions/' + potionId.toLowerCase())
         isPotionLoaded.value = false;
     });
 </script>
-
-
