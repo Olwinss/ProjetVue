eae5c233be2e5f0d69eb958bdb4a5f74e025ef3b
Olivier LABROSSE <labrosseolivier2004@gmail.com>
2024-03-21 09:09:29 +0000

    Pagination et recherche fonctionnelle via l'api

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -3,9 +3,9 @@
     <div id="title">
       <h1>Page des personnages</h1>
     </div>
-    <form id="search"> Search <input name="search" v-model="searchQuery">
-      <input type="number" v-model="desiredPage" min="1" :max="nbpage">
-      <button @click="goToPage">Go</button>
+    <form id="search">
+      <input type="text" v-model="filterName" placeholder="Entrez un nom de personnage">
+      <button @click="fetchData">Rechercher</button>
       <div id="pagination">
         <button @click="previousPage" :disabled="currentPage === 1">Previous</button>
         <span>{{ currentPage }}</span>
@@ -21,7 +21,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="character in filteredCharacters" :key="character.id">
+        <tr v-for="character in Characters" :key="character.id">
           <td>{{ character.attributes.name }}</td>
           <td>{{ character.attributes.born }}</td>
           <td>{{ character.attributes.house }}</td>
@@ -37,15 +37,14 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed} from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
 const Characters = ref([])
-const searchQuery = ref('')
-const itemsPerPage = 20 // un trop gros nombre ne fonctionnera pas pour la pagination
+const itemsPerPage = 20
 let currentPage = ref(1)
-const desiredPage = ref(1)
-let nbpage = ref(1);
+let nbpage = ref(1)
+const filterName = ref('')
 
 onMounted(async () => {
   try {
@@ -57,42 +56,26 @@ onMounted(async () => {
 
 async function fetchData() {
   try {
-    // Réinitialisation de la liste des personnages à chaque appel
     Characters.value = []
-    const response = await axios.get(`https://api.potterdb.com/v1/characters?page[size]=${itemsPerPage}&page[number]=${currentPage.value}`)
+    const response = await axios.get(`https://api.potterdb.com/v1/characters?filter[name_cont]=${filterName.value}&page[size]=${itemsPerPage}&page[number]=${currentPage.value}`)
     Characters.value = response.data.data
-    nbpage = Math.floor((response.data.meta.pagination.records+itemsPerPage)/itemsPerPage) // permet de s'assurer qu'un élement ne soit pas exclu 
+    nbpage = Math.ceil(response.data.meta.pagination.records / itemsPerPage)
   } catch (error) {
     console.error("Problème de chargement des Characters via l'api", error)
   }
 }
 
-const filteredCharacters = computed(() => {
-  return Characters.value.filter(character => {
-    return character.attributes.name.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
-      character.attributes.born.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
-      character.attributes.house.toLowerCase().includes(searchQuery.value.toLowerCase())
-  })
-})
-
 async function nextPage() {
   if (currentPage.value < nbpage) {
     currentPage.value++
-    await fetchData() // Appel de fetchData pour charger les données de la nouvelle page
+    await fetchData()
   }
 }
 
 async function previousPage() {
   if (currentPage.value > 1) {
     currentPage.value--
-    await fetchData() // Appel de fetchData pour charger les données de la nouvelle page
-  }
-}
-
-async function goToPage() {
-  if (desiredPage.value >= 1 && desiredPage.value <= nbpage) {
-    currentPage.value = desiredPage.value
-    await fetchData() // Appel de fetchData pour charger les données de la nouvelle page
+    await fetchData()
   }
 }
 </script>
