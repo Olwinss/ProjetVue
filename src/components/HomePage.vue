d91d88a7d1acd3a826d36eea734a3e080fd09621
Olivier LABROSSE <labrosseolivier2004@gmail.com>
2024-03-25 11:11:21 +0000

    Ajout d'une background image et modification du css

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -9,8 +9,8 @@ import '/public/homepage.css';
     <div id="title">
         <h1>Harry Potter Encyclopedia</h1>
         <p>
-            Ce site vous permet d'explorer l'univers d'Harry Potter par le biais d'une encyclopédie.
-            Vous retrouverez la liste des potions ainsi que leurs détails,la liste complète des livres et
+            Ce site vous permet d'explorer l'univers d'Harry Potter par le biais d'une encyclopédie. <br>
+            Vous retrouverez la liste des potions ainsi que leurs détails,la liste complète des , des sorts et
              finalement la liste des personnages de HP 
         </p>
     </div>
